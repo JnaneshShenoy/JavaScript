fe915dc61d018e120e23b75adb5af0514713d6e1
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2024-07-31 20:16:27 +0530

    email_updated

--- session vi/regex.js
+++ session vi/regex.js
@@ -11,11 +11,21 @@
 // let emailPattern = new RegExp("(?=.@*)(?=.+.com)");
 // let phonenumberpattern = new RegExp("?=[0-9]{10}");
 
-let emailPattern = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
-let emailString = prompt("Enter email");
+// let emailPattern = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+// let emailString = prompt("Enter email");
 
-if (!emailPattern.test(emailString)) {
-  alert("Email id is invalid. It should contain @ and a valid domain.");
+// if (!emailPattern.test(emailString)) {
+//   alert("Email id is invalid. It should contain @ and a valid domain.");
+// } else {
+//   alert("Email is valid");
+// }
+
+let emailPattern = new RegExp("(?=.@*)(?=.+.com)");
+//let phonenumberpattern-new RegExp("?=[0-9]{10}");
+//let passwordpattern=new RegExp("(?=.*[0-9]) (a-zA-Z)(?=.*[@#$%!^&*+_])");
+let emailString = prompt("enter email");
+if (emailPattern.test(emailString)) {
+alert("email is valid");
 } else {
-  alert("Email is valid");
+alert("email id is invalid it should contain @ and com");
 }
