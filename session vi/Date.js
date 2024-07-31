51ddd5d62accf2f2892b88f3837351d7c0cf08c4
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2024-07-31 19:30:19 +0530

    DayDate

---
+++ session vi/Date.js
@@ -0,0 +1,9 @@
+let dateObj = new Date();
+console.log("today's date is", dateObj);
+console.log("Date is " + dateObj.getDate());
+console.log("Day is " + dateObj.getDay());
+console.log("fullyear is " + dateObj.getFullYear());
+console.log("Hours is " + dateObj.getHours());
+console.log("Month is " + dateObj.getMonth());
+console.log("time is " + dateObj.getTime());
+console.log("milliseconds is " + dateObj.getMilliseconds());
