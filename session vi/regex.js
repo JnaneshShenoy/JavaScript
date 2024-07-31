3873da2dc9657e28eae8f7a79047c44b293e88a4
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2024-07-31 19:59:52 +0530

    regex

---
+++ session vi/regex.js
@@ -0,0 +1,8 @@
+// // Regex constructor
+// let myPat = new RegExp(pattern,modifiers);
+
+// // Regex literal
+// let myPat1 = /pattern/modifiers;
+
+let emailPattern = new RegExp("(?=.@*)(?=.+.com)");
+let phonenumberpattern = new RegExp("?=[0-9]{10}");
