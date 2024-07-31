f1cf895a15378d32cd939db4d7e803583f2283b9
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2024-07-31 19:44:29 +0530

    math functions

---
+++ session vi/Math.js
@@ -0,0 +1,7 @@
+console.log(Math.PI)
+console.log(Math.max(10, 20, 30, 40, 50));
+console.log(Math.min(10, 20, 30, 40, 50));
+console.log(Math.random());
+console.log(Math.round(30.5));
+console.log(Math.sqrt(9));
+console.log(Math.ceil(20.1));
